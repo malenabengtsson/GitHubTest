6ade61df7fe2991089989cf52697cf9f306d770c
Malena Bengtsson <malena94bengtsson@gmail.com>
2019-10-09 12:42:57 +0200

    it will

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello");
         System.out.println("uh oh!");
         System.out.println("Will Kraken notice?");
+        System.out.println("Yes, it will.")
     }
 }
